8e7899d344515f8061bae622ac180adbcc8560f7
Hardikjain101 <hardikjain.github@gmail.com>
2026-05-20 12:09:24 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'process.env.VITE_BACKEND_URL',
+        target: 'process.env.VITE_API_URL',
         changeOrigin: true,
       },
     },
